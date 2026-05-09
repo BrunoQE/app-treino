4c9607fc14bb41785812a455c0ac3c53e5de9a69
Bruno <brunoepucca@gmail.com>
2026-05-09 12:17:31 -0300

    ajuste

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import conectaNaDatabase from "./src/config/dbConnect.js";
 import routes from "./src/routes/index.js";
