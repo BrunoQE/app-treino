1c160b4280481b1dd00b6236f817d83a24c5c542
Bruno <brunoepucca@gmail.com>
2026-05-09 13:15:37 -0300

    backup

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
-    console.log('Todas as env:', JSON.stringify(process.env, null, 2));
     mongoose.connect(process.env.STRING_CONNECTION_DB);
                     
     return mongoose.connection;
