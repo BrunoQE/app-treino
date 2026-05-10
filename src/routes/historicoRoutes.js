4fd60ddc15408d9ee29231bf2439c7646fa7fd80
Bruno <brunoepucca@gmail.com>
2026-05-10 13:14:05 -0300

    ajustando nome da rota

--- src/routes/historicoRoutes.js
+++ src/routes/historicoRoutes.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 routes.get("/historico/streak", HistoricoController.buscarStreak);
 routes.get("/historico/stats", HistoricoController.buscarStats);
 routes.get("/historico", HistoricoController.listarHistorico);
-routes.get("/historico/recorders", HistoricoController.buscarRecordes);
+routes.get("/historico/recordes", HistoricoController.buscarRecordes);
 routes.post("/historico", HistoricoController.salvarHistorico);
 
 export default routes;
