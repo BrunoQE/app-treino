46653dbe8d8dd5ddef79bbc3dfa625ea37ece599
Bruno <brunoepucca@gmail.com>
2026-05-13 23:17:53 -0300

    ajustando rota de redefinir senha

--- src/routes/resetSenhaRoutes.js
+++ src/routes/resetSenhaRoutes.js
@@ -5,6 +5,6 @@ const routes = express.Router();
 
 routes.post('/auth/esqueci-senha', ResetSenhaController.esqueceuSenha);
 routes.post('/auth/verificar-codigo', ResetSenhaController.verificarCodigo);
-routes.post('auth/redefinir-senha', ResetSenhaController.redefinirSenha);
+routes.post('/auth/redefinir-senha', ResetSenhaController.redefinirSenha);
 
 export default routes;
