0e5b82207a4bf55303d6ac474c6d75bd11535151
Bruno <brunoepucca@gmail.com>
2026-05-23 01:48:19 -0300

    as rotas

--- src/routes/historicoRoutes.js
+++ src/routes/historicoRoutes.js
@@ -8,6 +8,9 @@ routes.get("/historico/stats", HistoricoController.buscarStats);
 routes.get("/historico", HistoricoController.listarHistorico);
 routes.get("/historico/recordes", HistoricoController.buscarRecordes);
 routes.get("/historico/evolucao", HistoricoController.buscarEvolucao);
+routes.get("/historico/volume-semanal", HistoricoController.buscarVolumeSemanal);
+routes.get("/historico/frequencia-mensal", HistoricoController.buscarFrequenciaMensal);
+routes.get("/historico/comparativo-semanal", HistoricoController.buscarComparativoSemanal);
 routes.get("/historico/sugestoes/:treinoId", HistoricoController.buscarSugestoesIA);
 routes.post("/historico", HistoricoController.salvarHistorico);
 
